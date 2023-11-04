515644be1efa1805c3f63137111d17c430082749
Austin-Cathey <austincatheyart@gmail.com>
2023-11-04 11:45:09 -0400

    updates, but not working, restarting comp

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const app = {
     data: {
-        url: "http://localhost:3000/notes/",
+        url: "http://localhost:3000/notes",
         notes:[]
         
     },
 //methods
-    getNotes: funtion() {
-        let container = document.getElementById('container')
+    getNotes: function() { 
+        /* let container = document.getElementById('container') */
         fetch(this.data.url, {
             method: 'GET',
             headers: {"Content-Type": "application/json"}
-        }).then(r => r.json())
+        })
+        .then(r => r.json())
         .then(response => {
            for (let note of response) {
             this.data.notes.push(note)
@@ -19,13 +20,23 @@ const app = {
         })
     },
 
-createNote: function() {},
-//post request to save note
+/* createNote: function(noteId) { 
+    fetch(this.data.url, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"}
+        body: JSON.stringify({"title": "Hi", "body": "COOL"})
+    })
+    .then(r => r.json())
+    .then(response => {
+        //finish
+        this.generateNotesHTML()
+    }) */
+},
 
-displayCreateForm: function() {},
-//displays blank form
+/* displayCreateForm: function() {},
+//displays blank form */
 
-deleteNote: function(noteId) { //from class 11-2
+/* deleteNote: function(noteId) { //from class 11-2
     fetch(this.data.url + noteId, {
         method: 'DELETE',
         headers: {"Content-Type": "application/json"}
@@ -35,10 +46,10 @@ deleteNote: function(noteId) { //from class 11-2
         //finish delete object from this.data.notes with id (with 'if')
         this.generateNotesHTML()
     })
-},
-// delete request, 
+}, */
 
-confirmDelete: function() {},
+
+/* confirmDelete: function() {},
 //display confirmation popup, call deleteNote
 editNote:function(id) {},
 // call displayEditForm, saves/overwrites note (request)
@@ -47,12 +58,13 @@ displayEditForm: function(note) {
     let form = document.getElementById('editForm');
      form.classList.remove('hidden') //with eventlistener to remove hidden
     //this should just be a variation of the display create form, but prepopulated
-},
-    generateNotesHTML: () => {
-        const container = document.getElementById('container');
+}, */
+
+    generateNotesHTML: function() {
+        const noteContainer = document.getElementById('container');
         for (let note of this.data.notes) {
-        container.innerHTML += `
-        <div class="noteCard>
+        noteContainer.innerHTML += `
+        <div class="noteCard">
             <div>${note.title}</div>
             <div>${note.body}</div>
             <button class="editButton data-id=${note.id}>Edit</button>
